ae5a7040e7adb3a197c887dc8ca3b7992c557aee
@hexlet/cli <support@hexlet.io>
2021-04-13 21:28:26 +0200

    @hexlet/cli: submit

---
+++ exercises/unit-testing/.eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+    'jest/globals': true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  plugins: [
+    'jest',
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  rules: {
+  },
+};
--- exercises/unit-testing/__tests__/index.test.js
+++ exercises/unit-testing/__tests__/index.test.js
@@ -1,23 +1,25 @@
-describe("object assign", () => {
-  it("should return union object", () => {
-    const src = { k: "v", b: "b" };
-    const target = { k: "v2", a: "a" };
+describe('object assign', () => {
+  it('should return union object', () => {
+    const src = { k: 'v', b: 'b' };
+    const target = { k: 'v2', a: 'a' };
     const result = Object.assign(target, src);
-    const expected = { a: "a", b: "b", k: "v" };
+    const expected = { a: 'a', b: 'b', k: 'v' };
 
     expect(result).toEqual(expected);
   });
 
-  it("should return nested object", () => {
+  it('should return nested object', () => {
     const src = { a: 1, b: { c: 2 } };
-    const target = { d: "test", e: { f: { g: 123 } } };
+    const target = { d: 'test', e: { f: { g: 123 } } };
     const result = Object.assign(target, src);
-    const expected = { a: 1, b: { c: 2 }, d: "test", e: { f: { g: 123 } } };
+    const expected = {
+      a: 1, b: { c: 2 }, d: 'test', e: { f: { g: 123 } },
+    };
 
     expect(result).toEqual(expected);
   });
 
-  it("should return target when source null", () => {
+  it('should return target when source null', () => {
     const src = null;
     const target = { a: 1 };
     const result = Object.assign(target, src);
@@ -26,18 +28,20 @@ describe("object assign", () => {
     expect(result).toEqual(expected);
   });
 
-  it("should return union of objects", () => {
+  it('should return union of objects', () => {
     const src = { b: 2 };
     const src1 = { c: 3 };
     const src2 = { d: 4 };
     const target = { a: 1 };
     const result = Object.assign(target, src, src1, src2);
-    const expected = { a: 1, b: 2, c: 3, d: 4 };
+    const expected = {
+      a: 1, b: 2, c: 3, d: 4,
+    };
 
     expect(result).toEqual(expected);
   });
 
-  it("should return empty object", () => {
+  it('should return empty object', () => {
     const src = {};
     const target = {};
     const result = Object.assign(target, src);
