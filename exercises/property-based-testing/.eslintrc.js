9953f218c973d7adbaed128565787d2a14b2ffc8
@hexlet/cli <support@hexlet.io>
2021-04-15 21:17:31 +0200

    @hexlet/cli: submit

--- exercises/property-based-testing/.eslintrc.js
+++ exercises/property-based-testing/.eslintrc.js
@@ -2,10 +2,10 @@ module.exports = {
   env: {
     es2021: true,
     node: true,
-    "jest/globals": true,
+    'jest/globals': true,
   },
-  extends: ["airbnb-base", "plugin:jest/recommended"],
-  plugins: ["jest"],
+  extends: ['airbnb-base', 'plugin:jest/recommended'],
+  plugins: ['jest'],
   parserOptions: {
     ecmaVersion: 12,
   },
--- exercises/property-based-testing/__tests__/index.test.js
+++ exercises/property-based-testing/__tests__/index.test.js
@@ -1,47 +1,42 @@
-const fc = require("fast-check");
+const fc = require('fast-check');
 
 const sort = (data) => data.slice().sort((a, b) => a - b);
+const isEquals = (a, b) => a.length === b.length && a.every((v) => b.includes(v));
 
-describe("array sort", () => {
-  it("should return sorted array", () => {
+describe('array sort', () => {
+  it('should return sorted array', () => {
     fc.assert(
       fc.property(fc.int16Array(), (data) => {
         const result = sort(data);
 
         expect(result).toBeSorted();
-      })
+      }),
     );
   });
 
-  it("should return same length", () => {
+  it('should return same length', () => {
     fc.assert(
       fc.property(fc.int16Array(), (data) => {
         const result = sort(data).length;
         const expected = data.length;
 
         expect(result).toEqual(expected);
-      })
+      }),
     );
   });
 
-  it("should return array with same items", () => {
-    const count = (tab, element) => tab.filter((v) => v === element).length;
-
+  it('should return array with same items', () => {
     fc.assert(
       fc.property(fc.int16Array(), (data) => {
         const sorted = sort(data);
+        const result = isEquals(sorted, data);
 
-        for (const item of data) {
-          const result = count(sorted, item);
-          const expected = count(data, item);
-
-          expect(result).toEqual(expected);
-        }
-      })
+        expect(result).toBeTruthy();
+      }),
     );
   });
 
-  it("should return empty arrray", () => {
+  it('should return empty arrray', () => {
     const result = sort([]);
     const expected = [];
 
