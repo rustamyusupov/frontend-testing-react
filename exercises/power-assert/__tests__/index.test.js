8bb324a7af409328fba262766339abd85025517a
@hexlet/cli <support@hexlet.io>
2021-04-29 20:45:37 +0200

    @hexlet/cli: submit

--- exercises/power-assert/__tests__/index.test.js
+++ exercises/power-assert/__tests__/index.test.js
@@ -8,5 +8,5 @@ assert.deepEqual(flattenDepth(array), [1, 2, [3, [4]], 5], 'default depth');
 assert.deepEqual(flattenDepth(array, 1), [1, 2, [3, [4]], 5], '1 depth');
 assert.deepEqual(flattenDepth(array, 2), [1, 2, 3, [4], 5], '2 depth');
 assert.deepEqual(flattenDepth(array, 3), [1, 2, 3, 4, 5], '3 depth');
-assert.deepEqual(flattenDepth([]), [], 'should return empty array');
+assert.deepEqual(flattenDepth([]), [], 'empty array');
 // END
