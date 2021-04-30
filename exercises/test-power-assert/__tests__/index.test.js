6abe5883362666b31be7900347886a4673e90743
@hexlet/cli <support@hexlet.io>
2021-04-30 20:27:08 +0200

    @hexlet/cli: submit

--- exercises/test-power-assert/__tests__/index.test.js
+++ exercises/test-power-assert/__tests__/index.test.js
@@ -3,10 +3,16 @@ const { flattenDepth } = require('lodash');
 
 // BEGIN
 const array = [1, [2, [3, [4]], 5]];
+const cases = [
+  // data depth      expected             description
+  [array, undefined, [1, 2, [3, [4]], 5], 'default depth'],
+  [array, 1, [1, 2, [3, [4]], 5], '1 depth'],
+  [array, 2, [1, 2, 3, [4], 5], '2 depth'],
+  [array, 3, [1, 2, 3, 4, 5], '3 depth'],
+  [[], undefined, [], 'empty array'],
+];
 
-assert.deepEqual(flattenDepth(array), [1, 2, [3, [4]], 5], 'default depth');
-assert.deepEqual(flattenDepth(array, 1), [1, 2, [3, [4]], 5], '1 depth');
-assert.deepEqual(flattenDepth(array, 2), [1, 2, 3, [4], 5], '2 depth');
-assert.deepEqual(flattenDepth(array, 3), [1, 2, 3, 4, 5], '3 depth');
-assert.deepEqual(flattenDepth([]), [], 'empty array');
+cases.forEach(([data, depth, expected, description]) =>
+  assert.deepEqual(flattenDepth(data, depth), expected, description)
+);
 // END
