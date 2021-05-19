0d263b5727acecb081c97ede358c6dce149d5b1b
@hexlet/cli <support@hexlet.io>
2021-05-19 22:06:58 +0200

    @hexlet/cli: submit

--- exercises/e2e-testing/__tests__/index.test.js
+++ exercises/e2e-testing/__tests__/index.test.js
@@ -57,13 +57,10 @@ describe('simple blog', () => {
     await page.click(
       '[data-testid="article"]:first-child [data-testid^="article-edit-link"]',
     );
-    await page.waitForSelector('[data-testid="article-edit-form"]');
-    await expect(page).toFillForm('form[data-testid="article-edit-form"]', {
-      'article[name]': name,
-    });
-    await expect(page).toClick('[data-testid="article-update-button"]');
+    await expect(page).toFill('#name', name);
+    await page.click('[data-testid="article-update-button"]');
     await page.waitForSelector('[data-testid="articles"]');
-
+  
     expect(page).toMatch(name);
   });
 });
