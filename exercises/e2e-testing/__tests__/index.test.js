133fc239a2c4e59ebd42d691b54484cdf64825c4
@hexlet/cli <support@hexlet.io>
2021-05-19 21:27:49 +0200

    @hexlet/cli: submit

---
+++ exercises/e2e-testing/__tests__/index.test.js
@@ -0,0 +1,68 @@
+// BEGIN
+require('expect-puppeteer');
+
+const faker = require('faker');
+
+const port = 5000;
+const appUrl = `http://localhost:${port}`;
+
+describe('simple blog', () => {
+  beforeAll(async () => {
+    await page.goto(appUrl);
+  });
+
+  it('should open main page', async () => {
+    const expected = 'Welcome to a Simple blog!';
+
+    await expect(page).toMatch(expected);
+  });
+
+  it('should return articles', async () => {
+    await page.goto(`${appUrl}/articles`);
+    const articles = await page.$$('[data-testid="article"]');
+    const result = articles.length;
+    const expected = 0;
+
+    expect(result).toBeGreaterThan(expected);
+  });
+
+  it('should open form', async () => {
+    await page.goto(`${appUrl}/articles`);
+    await page.click('[data-testid="article-create-link"]');
+    const result = await page.waitForSelector('form');
+
+    expect(result).toBeTruthy();
+  });
+
+  it('should create new article', async () => {
+    const name = faker.lorem.sentence();
+    const content = faker.lorem.paragraph();
+
+    await page.goto(`${appUrl}/articles/new`);
+    await expect(page).toFillForm('form[data-testid="article-create-form"]', {
+      'article[name]': name,
+      'article[content]': content,
+    });
+    await expect(page).toSelect('data-testid="article-category"', '1');
+    await expect(page).toClick('[data-testid="article-create-button"]');
+    await page.waitForSelector('[data-testid="articles"]');
+
+    expect(page).toMatch(name);
+  });
+
+  it('should edit article', async () => {
+    const name = faker.lorem.sentence();
+
+    await page.goto(`${appUrl}/articles`);
+    await page.click('tbody > tr:nth-child(1) > td:nth-child(4) > a');
+    await page.waitForSelector('#edit-form');
+    await expect(page).toFillForm('form[data-testid="article-create-form"]', {
+      'article[name]': name,
+    });
+    await expect(page).toClick('[data-testid="article-create-button"]');
+    await page.waitForSelector('[data-testid="articles"]');
+
+    expect(page).toMatch(name);
+  });
+});
+// END
