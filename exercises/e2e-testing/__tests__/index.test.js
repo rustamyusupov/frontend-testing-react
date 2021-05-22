165c3483b55ef8d8bcc111d5e04a5da920d54ceb
@hexlet/cli <support@hexlet.io>
2021-05-22 14:46:38 +0200

    @hexlet/cli: submit

--- exercises/e2e-testing/__tests__/index.test.js
+++ exercises/e2e-testing/__tests__/index.test.js
@@ -49,5 +49,20 @@ describe('simple blog', () => {
 
     expect(page).toMatch(name);
   });
+
+  it('should edit article', async () => {
+    const name = faker.lorem.sentence();
+
+    await page.goto(`${appUrl}/articles`);
+    await page.click(
+      '[data-testid="article"]:first-child [data-testid^="article-edit-link"]',
+    );
+    await page.waitForSelector('[data-testid="article-edit-form"]');
+    await expect(page).toFill('[data-testid="article-name"]', name);
+    await page.click('[data-testid="article-update-button"]');
+    await page.waitForSelector('[data-testid="articles"]');
+
+    expect(page).toMatch(name);
+  });
 });
 // END
