9137570d43812c63fa43568bf5b582f049be7ea8
@hexlet/cli <support@hexlet.io>
2021-06-10 13:03:01 +0200

    @hexlet/cli: submit

--- exercises/testing-library-react/__tests__/test.jsx
+++ exercises/testing-library-react/__tests__/test.jsx
@@ -6,7 +6,12 @@ import nock from 'nock';
 import React from 'react';
 import { render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+import {
+  flow, map, toLower, filter, startsWith, capitalize,
+} from 'lodash/fp';
 import Autocomplete from '../src/Autocomplete.jsx';
+
+// BEGIN
 
 const host = 'http://localhost';
 
@@ -14,6 +19,48 @@ beforeAll(() => {
   nock.disableNetConnect();
 });
 
-// BEGIN
+const countries = ['Albania', 'Algeria', 'Armenia'];
+const getResponse = (char) => flow(map(toLower), filter(startsWith(char)), map(capitalize));
 
+describe('autocomplete', () => {
+  it('should render countries', async () => {
+    ['a', 'al', 'alg'].forEach((char) => {
+      nock(host)
+        .get('/countries')
+        .query({ term: char })
+        .reply(200, getResponse(char)(countries));
+    });
+
+    const { container } = render(<Autocomplete />);
+    const input = screen.getByRole('textbox');
+
+    userEvent.type(input, 'a');
+
+    await waitFor(() => {
+      expect(screen.queryByText('Albania')).toBeVisible();
+      expect(screen.queryByText('Algeria')).toBeVisible();
+      expect(screen.queryByText('Armenia')).toBeVisible();
+    });
+
+    userEvent.type(input, 'l');
+
+    await waitFor(() => {
+      expect(screen.queryByText('Albania')).toBeVisible();
+      expect(screen.queryByText('Algeria')).toBeVisible();
+      expect(screen.queryByText('Armenia')).toBeNull();
+    });
+
+    userEvent.type(input, 'g');
+
+    await waitFor(() => {
+      expect(screen.queryByText('Albania')).toBeNull();
+      expect(screen.queryByText('Algeria')).toBeVisible();
+      expect(screen.queryByText('Armenia')).toBeNull();
+    });
+
+    userEvent.clear(input);
+
+    expect(container.querySelector('ul')).toBeNull();
+  });
+});
 // END
